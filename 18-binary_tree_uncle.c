3d63d48f0a77a4ccd513ce2ed08eff7da47d24b7
ROgbuagu05 <chizzy3003@gmail.com>
2023-08-29 04:12:41 -0700

    8-binary_tree_postorder.c

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+/**
+ * binary_tree_uncle - A function that finds the uncle of a node.
+ * @node: A pointer to the node to find the uncle.
+ * Return: A pointer to the uncle node
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (node == NULL)
+	{
+		return (NULL);
+	}
+	if (node->parent == NULL)
+	{
+		return (NULL);
+	}
+	if (node->parent->parent == NULL)
+	{
+		return (NULL);
+	}
+	if (node->parent->parent->left == node->parent)
+	{
+		return (node->parent->parent->right);
+	}
+	return (node->parent->parent->left);
+}
