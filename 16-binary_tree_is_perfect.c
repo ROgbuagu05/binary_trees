4d72cee3c594bc2d207f4838a2d6910f5fcf084a
ROgbuagu05 <chizzy3003@gmail.com>
2023-08-29 04:47:48 -0700

    16-binary_tree_is_perfect.c

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -15,11 +15,22 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	{
 		return (0);
 	}
-
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
-
-	return (1 + (left_height > right_height ? left_height : right_height));
+	else
+	{
+		if (tree->left != NULL)
+		{
+			left_height = 1 + binary_tree_height(tree->left);
+		}
+		if (tree->right != NULL)
+		{
+			right_height = 1 + binary_tree_height(tree->right);
+		}
+		if (left_height >= right_height)
+		{
+			return (left_height);
+		}
+		return (right_height);
+	}
 }
 
 /**
