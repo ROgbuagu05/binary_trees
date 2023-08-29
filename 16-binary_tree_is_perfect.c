cf4c52268273f065042bcb60bdb05516be4f0658
ROgbuagu05 <chizzy3003@gmail.com>
2023-08-29 04:52:43 -0700

    16-binary_tree_is_perfect.c

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -24,12 +24,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		if (tree->right != NULL)
 		{
 			right_height = 1 + binary_tree_height(tree->right);
-		}
-		if (left_height >= right_height)
-		{
-			return (left_height);
-		}
-		return (right_height);
+		}	
+		return (left_heigth + right_height);
 	}
 }
 
